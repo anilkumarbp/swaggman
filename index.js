f856dec5989d38b472e5140fde3a57ea52947d6a
Benjamin Dean <ben@benjamindean.com>
2016-11-18 05:26:05 -0800

    Add the conversion from Swagger Tags to Postman Folders

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 'use strict';
+/**
+  * Postman schema reference (https://schema.getpostman.com/json/collection/v2/docs/)
+  *
+  * variables (array) of Variable objects - Variables can be defined and referenced from any part of a request
+    * id (string) - Variable ID is a unique user-defined value to identify the variable within a collection (similar to var `name`)
+    * value (JS Data Type) - The value that a variable holds in this collection.
+    * type (string) - Can have multiple types, this field specifies the type of the variable
+    * name (string) - Variable name (used in the replacement strings)
+  * info (object) - Meta data about the collection
+    * name (string) - Collection Name
+    * _postman_id (string) - Postman Collection ID (load from environment variable)
+    * description (string) - Describe the Postman Collection
+    * schema (string) - https://schema.getpostman.com/json/collection/v2.0.0/collection.json
+    * version (string) - semver format
+  * items (array) of Folders in the Postman Collection
+    * name (string) - 
+    * description (string) - 
+    * items (array) of API Requests
+    * auth (object)
+  * events (array) of Event Objects
+    * listen (string) - Can be set to `test` or `prerequest` for test scripts or pre-request scripts respectively
+    * script (string) - JS code used to perform setup/teardown operations on a particular response
+    * disabled (boolean) - Is the event disabled? Defaults to enabled
+**/
+
+/**
+  * Swagger schema reference (http://swagger.io/specification/)
+  *
+  * swagger (string) - version of Swagger itself
+  * info (object) - Meta data about the spec
+  * host (string) - Host portion of URI
+  * basePath (string) - Base path where API is served relative to host
+  * schemes (string) - Transfer protocol of the API
+  * consumes (string) - List of MIME types the APIs can consume (global to all APIs, but can be overridden on specific API calls)
+  * produces (string) - Inverse of consumes
+  * paths [Paths Object] - The available paths and operations for the API
+  * definitions [Definitions Object] - An object to hold data types produced and consumed by operations
+  * parameters [Parameters Definitions Object] - Object to hold parameters that can be used across operations (does not define global params for all ops)
+  * responses [Responses Definitions Object] - Object to hold responses that can be used across operations (does not define global responses for all ops)
+  * securityDefinitions [securityDefinitionsObject] - Security scheme definitions that can be used across the spec
+  * tags [Tag Object] - List of tags used by spec with additional metadata. Order of tags can be used to reflect on their order by the parsing tools. Not all tags used by Operation Object must be declared. Undeclared tags may be organized randomly or based on tool logic. Must be unique 
+  * externalDocs ExternalDocumentationObject - Additional external docs
+**/
+
 
 // Deps
 require('dotenv').config();
@@ -33,36 +77,50 @@ const getSwaggerFromUri = function(url) {
 };
 
 // Build the Postman Collection Meta Data
-const createCollectionMeta = function(swaggerJSON, postman) {
+const createMeta = function(swagg, postman) {
+    console.log('createMeta');
+    postman       = postman || {};
     try {
         // Define base Postman schema
-        const postman       = postman || {};
         postman.info        = {};
         postman.variables   = [];
         postman.items       = [];
         postman.events      = [];
 
         // Define Postman Info from Environment Variables but default to Swagger properties
-        postman.info.name           = (process.env.TITLE) ? process.env.TITLE : swaggerJSON.info.title;
-        postman.info.description    = (process.env.DESCRIPTION) ? process.env.DESCRIPTION : swaggerJSON.info.description;
-        postman.info.version        = (process.env.POSTMAN_SCHEMA_VERSION) ? process.env.POSTMAN_SCHEMA_VERSION : swaggerJSON.info.version;
+        postman.info.name           = (process.env.TITLE) ? process.env.TITLE : swagg.info.title;
+        postman.info.description    = (process.env.DESCRIPTION) ? process.env.DESCRIPTION : swagg.info.description;
+        postman.info.version        = (process.env.POSTMAN_SCHEMA_VERSION) ? process.env.POSTMAN_SCHEMA_VERSION : swagg.info.version;
         postman.info.schema         = process.env.POSTMAN_SCHEMA_Uri || "https://schema.getpostman.com/json/collection/v2.0.0/collection.json";
     } catch(e) {
         return Promise.reject(e);
     }
 
-    return Promise.resolve(swaggerJSON, postman);
+    return Promise.resolve({swg: swagg, pm:postman});
 }
 
 // Build the Postman Collection Directory Structures
-const createCollectionDirs = function(swaggerJSON, postman) {
+const createFolders = function(config) {
+    console.log('createDirs');
     try {
+        let folders = [];
+        config.swg.tags.forEach((tag) => {
+            console.log('Tag: ', tag);
+            let folder = {};
+            if(tag.name) folder.name = tag.name;
+            if(tag.description) folder.description = tag.description;
+            if(tag.externalDocs) {
+                folder.description += '\n\n';
+                folder.description += 'Additional Information: ' + tag.externalDocs.description + ' - ' + tag.externalDocs.url;
+            }
+            config.pm.items.push(folder);
+        });
 
     } catch(e) {
         return Promise.reject(e)
     }
     
-    return Promise.resolve(postman);
+    return Promise.resolve(config);
 }
 
 // Write Postman Collection JSON to filename specified
@@ -79,9 +137,9 @@ getSwaggerFromUri(swaggerSpecUri)
 .then((swagg) => {
     return JSON.parse(swagg);
 })
-.then(createCollectionMeta)
-.then(createCollectionDirs)
-.then((postmanDirs) => console.log(postmanDirs))
+.then(createMeta)
+.then(createFolders)
+.then((config) => console.log(config.pm))
 .catch((err) => console.error(err));
 
 function writePostmanFile(json) {
@@ -101,31 +159,6 @@ function writePostmanFile(json) {
         }
     }
 }
-
-/**
-  * Postman schema reference (https://schema.getpostman.com/json/collection/v2/docs/)
-  *
-  * variables (array) of Variable objects - Variables can be defined and referenced from any part of a request
-    * id (string) - Variable ID is a unique user-defined value to identify the variable within a collection (similar to var `name`)
-    * value (JS Data Type) - The value that a variable holds in this collection.
-    * type (string) - Can have multiple types, this field specifies the type of the variable
-    * name (string) - Variable name (used in the replacement strings)
-  * info (object)
-    * name (string) - Collection Name
-    * _postman_id (string) - Postman Collection ID (load from environment variable)
-    * description (string) - Describe the Postman Collection
-    * schema (string) - https://schema.getpostman.com/json/collection/v2.0.0/collection.json
-    * version (string) - semver format
-  * items (array) of Folders in the Postman Collection
-    * name (string) - 
-    * description (string) - 
-    * items (array) of API Requests
-    * auth (object)
-  * events (array) of Event Objects
-    * listen (string) - Can be set to `test` or `prerequest` for test scripts or pre-request scripts respectively
-    * script (string) - JS code used to perform setup/teardown operations on a particular response
-    * disabled (boolean) - Is the event disabled? Defaults to enabled
-**/
 
 function convert(swaggerJSON, cb) {
     if(!swaggerJSON || !cb) {
